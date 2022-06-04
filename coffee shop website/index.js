237fdf41950133c382f64bdbfd78988f9f8c74ac
ElyasShamal <89407707+ElyasShamal@users.noreply.github.com>
2022-06-04 11:45:59 -0400

    coffee shop website

---
+++ coffee shop website/index.js
@@ -0,0 +1,17 @@
+
+
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+navbar.classList.toggle('active');
+searchForm.classList.remove('active');
+
+
+}
+
+
+
+let searchForm =document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+searchForm.classList.toggle('active');
+navbar.classList.remove('active');
+}
